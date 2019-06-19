29982d3086b44142e61379ce03c3be9746b843ad
Rest11 <shynkarenko.max@gmail.com>
2019-06-19 15:25:10 +0300

    added note about Google Auth

--- src/app/services/google-auth.service.ts
+++ src/app/services/google-auth.service.ts
@@ -1,3 +1,12 @@
+/*
+    NOTE:
+    We do not want to use refresh token because Google sends you refreshing token if a mode was set "offline" and only once.
+    Because Google passes a refresh token only at the time when the user authorizes your application via a consent screen.
+    After that when the user tries to login again, he will not see the consent screen.
+    No consent means no refresh token passed by Google.
+    Google does not recommend it because users are able to permit access to there accounts by themselves (sign in).
+ */
+
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
 
@@ -40,7 +49,7 @@ export class GoogleAuthService {
                 return;
             }
 
-            this.auth2.isSignedIn.listen(async (signedIn) => {
+            this.auth2.isSignedIn.listen((signedIn) => {
                 if (signedIn) resolve(this.userGoogleToken);
                 else reject(null);
             });
